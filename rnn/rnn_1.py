9360f31dadc7c55cc3138075d4588967150e486e
Vu Anh <anhv.ict91@gmail.com>
2018-08-02 14:21:06 +0700

    add rnn

--- rnn/rnn_1.py
+++ rnn/rnn_1.py
@@ -1,9 +1,12 @@
 import torch
 from torch import nn
+import numpy as np
 
-rnn = nn.RNN(10, 20, 2)
+
+
+rnn = nn.RNN(10, 20, 1)
 input = torch.randn(5, 3, 10)
-h0 = torch.randn(2, 3, 20)
+h0 = torch.randn(1, 3, 20)
 output, hn = rnn(input, h0)
 print(output)
 print(hn)
